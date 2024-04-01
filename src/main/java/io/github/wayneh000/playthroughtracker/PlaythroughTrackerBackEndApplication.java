97d6851e79c5cf5637662c43fd1e477687ff15ff
Wayne He <70216125+wayneh000@users.noreply.github.com>
2024-03-31 21:44:20 -0400

    Initial commit.

---
+++ src/main/java/io/github/wayneh000/playthroughtracker/PlaythroughTrackerBackEndApplication.java
@@ -0,0 +1,13 @@
+package io.github.wayneh000.playthroughtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaythroughTrackerBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaythroughTrackerBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wayneh000/playthroughtracker/PlaythroughTrackerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wayneh000.playthroughtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaythroughTrackerBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
