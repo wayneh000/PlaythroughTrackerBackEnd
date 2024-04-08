90583b46816b8c567c09b6066b1a5ab586d52c40
Wayne He <70216125+wayneh000@users.noreply.github.com>
2024-04-07 22:31:19 -0400

    Added request POJOs.

---
+++ src/main/java/io/github/wayneh000/playthroughtracker/request/NewAccountRequest.java
@@ -0,0 +1,23 @@
+package io.github.wayneh000.playthroughtracker.request;
+
+public class NewAccountRequest {
+
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/main/java/io/github/wayneh000/playthroughtracker/request/UpdateAccountRequest.java
@@ -0,0 +1,41 @@
+package io.github.wayneh000.playthroughtracker.request;
+
+public class UpdateAccountRequest {
+
+	private Integer id;
+	private String username;
+	private String oldPassword;
+	private String newPassword;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getOldPassword() {
+		return oldPassword;
+	}
+	
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+	
+	public String getNewPassword() {
+		return newPassword;
+	}
+	
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+}
