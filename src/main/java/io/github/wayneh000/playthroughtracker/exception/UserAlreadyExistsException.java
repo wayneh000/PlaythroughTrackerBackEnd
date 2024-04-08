cc7f36ec6e8d40247767c14194d7865bbad94c7d
Wayne He <70216125+wayneh000@users.noreply.github.com>
2024-04-08 00:13:21 -0400

    Renamed exception.

--- src/main/java/io/github/wayneh000/playthroughtracker/exception/UserAlreadyExistsException.java
+++ src/main/java/io/github/wayneh000/playthroughtracker/exception/UserAlreadyExistsException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.CONFLICT, reason = "Username already exists!")
-public class DuplicateUserException extends PlaythroughTrackerException {
+public class UserAlreadyExistsException extends PlaythroughTrackerException {
 
 }
