371568a838f21d039424dbf5ff82f1664130173e
Wayne He <70216125+wayneh000@users.noreply.github.com>
2024-04-09 01:24:24 -0400

    Corrected HTTP Status Code for invalid credentials.

--- src/main/java/io/github/wayneh000/playthroughtracker/exception/InvalidCredentialsException.java
+++ src/main/java/io/github/wayneh000/playthroughtracker/exception/InvalidCredentialsException.java
@@ -3,7 +3,7 @@ package io.github.wayneh000.playthroughtracker.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Invalid username or password. ")
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Invalid username or password. ")
 public class InvalidCredentialsException extends PlaythroughTrackerException {
 
 }
